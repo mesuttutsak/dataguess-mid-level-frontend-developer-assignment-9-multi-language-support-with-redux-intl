13f4e0242159ece8dba5abd37562ba0a5e3a7684
Mesut Tutsak <ttsk.mesut@gmail.com>
2023-09-18 04:55:11 +0300

    Application UI was completed.

--- src/core/components/Header/index.tsx
+++ src/core/components/Header/index.tsx
@@ -1,32 +1,54 @@
 import LanguageSelector from "../LanguageSelector";
 import Text from "../Text";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation, useParams } from "react-router-dom";
 import styles from "./header.module.scss";
 import { useSelector } from "react-redux";
 import { AppState } from "../../store/reducer";
+import { renderClasses } from "../../utils/renderClasses";
+import { useEffect } from "react";
 
 const Header = () => {
-    const {localeLang, browserLang} = useSelector((state:AppState) => state)
+    const { localeLang, browserLang } = useSelector((state: AppState) => state)
+    const location = useLocation();
+    const pathname = location.pathname;
+
+    function isActivePage(isPath: string) {
+        let active = true;
+        const pathArr = pathname.split('/').filter((item: string) => item !== "");
+        console.log(pathArr);
+        if ((isPath === 'home') && ((pathArr[0] === localeLang && pathArr.length === 1) || pathArr.length === 0)) {
+            return active;
+        } else if ((isPath === 'about') && (pathArr[1] === isPath || pathArr[0] === isPath)) {
+            return active;
+        }
+    }
+
     return (
         <header>
             <div className='_container'>
                 <div className={styles.headerWrap}>
-                    <span className={styles.left}>
-                        <Text fontWeight="bold" fontSize="lg" tag="h1">Dataguess Multi-Lang Case - {localeLang}</Text>
+                    <div className={styles.top}>
+                        <div className={styles.left}>
+                            <Text fontWeight="bold" fontSize="lg" tag="h1"><Link to={'https://www.dataguess.com/career-mid-level-frontend-developer-assignment'} target="_blank">Dataguess Multi-Lang Case</Link></Text>
+                        </div>
+                        <div className={styles.right}>
+                            <Text fontWeight="normal" fontSize="lg" tag="h1"><Link to={'https://www.mesuttutsak.dev/'} target="_blank">Mesut Tutsak - Frontend Developer</Link></Text>
+                        </div>
+                    </div>
+                    <div className={styles.bottom}>
 
-                        <nav>
-                            <NavLink to={`/${localeLang === browserLang ? '' : localeLang }`}>Home</NavLink>
-                            <NavLink to={`${localeLang === browserLang ? '' : '/'+localeLang}/about`}>About</NavLink>
+                        <nav className={styles.left}>
+                            <NavLink className={isActivePage('home') ? styles.active : ""} to={`/${localeLang === browserLang ? '' : localeLang}`}>Home</NavLink>
+                            <NavLink className={isActivePage('about') ? styles.active : ""} to={`${localeLang === browserLang ? '' : '/' + localeLang}/about`}>About</NavLink>
                         </nav>
-                    </span>
-                    
 
-                    <div>
-                        <LanguageSelector />
+                        <div className={styles.right}>
+                            <LanguageSelector />
+                        </div>
                     </div>
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
 
--- src/core/components/LanguageSelector/index.tsx
+++ src/core/components/LanguageSelector/index.tsx
@@ -1,7 +1,12 @@
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+
+import styles from "./language.module.scss";
+
 import { AppState } from '../../store/reducer';
+
 import { useLanguageChange } from '../../hooks/useLanguageChange';
+import { renderClasses } from '../../utils/renderClasses';
 
 function LanguageSelector() {
   const { localeLang, browserLang } = useSelector((state: AppState) => state)
@@ -10,10 +15,10 @@ function LanguageSelector() {
   const handleChangeLanguage = useLanguageChange();
 
   return (
-    <div>
-      <button onClick={() => handleChangeLanguage('en', pathname, browserLang, localeLang)}>English</button>
-      <button onClick={() => handleChangeLanguage('es', pathname, browserLang, localeLang)}>Español</button>
-      <button onClick={() => handleChangeLanguage('tr', pathname, browserLang, localeLang)}>Türkçe</button>
+    <div className={styles.langSelector}>
+      <button className={renderClasses([localeLang === 'en' ? styles.active : ''])} onClick={() => handleChangeLanguage('en', pathname, browserLang, localeLang)}>EN</button>
+      <button className={renderClasses([localeLang === 'es' ? styles.active : ''])} onClick={() => handleChangeLanguage('es', pathname, browserLang, localeLang)}>ES</button>
+      <button className={renderClasses([localeLang === 'tr' ? styles.active : ''])} onClick={() => handleChangeLanguage('tr', pathname, browserLang, localeLang)}>TR</button>
     </div>
   );
 }
